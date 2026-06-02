988d6e76af78070ff4ab2f19f57cd93477d088ad
Joseph <joseph1marzok@gmail.com>
2026-06-02 15:47:31 +0300

    c

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable /tmp/flutter
-export PATH="$PATH:/tmp/flutter/bin"
-
-flutter pub get
-flutter build web --release
